030fe6e6ab53a32ad390dfcd5827dff8e470a6a1
Dan Roach <droach282@gmail.com>
2018-07-28 06:14:25 -0600

    Added project scaffold

---
+++ YNAB.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace YNAB.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ YNAB/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace YNAB
+{
+    public class Class1
+    {
+    }
+}
